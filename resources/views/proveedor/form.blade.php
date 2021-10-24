c57e5e45313bcda2a62d9df79f00398f224d671f
carlitosalc1 <carlos_a002@hotmail.com>
2021-10-23 20:16:06 -0500

    OK

--- resources/views/proveedor/form.blade.php
+++ resources/views/proveedor/form.blade.php
@@ -18,27 +18,27 @@
 </div>
 <div class="form-group">
 <label for="Apellido"> Apellido </label>
-<input type="text" class="form-control" name="apellido" value="{{ isset($producto->apellido) ? $producto->apellido : ''  }}" id="apellido">
+<input type="text" class="form-control" name="apellido" value="{{ isset($proveedor->apellido) ? $proveedor->apellido : ''  }}" id="apellido">
 </div>
 <div class="form-group">
 <label for="Razon Social"> Razon Social </label>
-<input type="text" class="form-control" name="razon_social" value=" {{ isset($producto->razon_social ) ? $producto->razon_social : ''  }}" id="razon_social">
+<input type="text" class="form-control" name="razon_social" value=" {{ isset($proveedor->razon_social ) ? $proveedor->razon_social : ''  }}" id="razon_social">
 </div>
 <div class="form-group">
 <label for="Nit"> Nit </label>
-<input type="text" class="form-control" name="nit" value="{{ isset($producto->nit) ? $producto->nit : ''  }}" id="nit">
+<input type="text" class="form-control" name="nit" value="{{ isset($proveedor->nit) ? $proveedor->nit : ''  }}" id="nit">
 </div>
 <div class="form-group">
 <label for="Direccion"> Direccion </label>
-<input type="text" class="form-control" name="direccion" value="{{ isset($producto->direccion) ? $producto->direccion : ''  }}" id="direccion">
+<input type="text" class="form-control" name="direccion" value="{{ isset($proveedor->direccion) ? $proveedor->direccion : ''  }}" id="direccion">
 </div>
 <div class="form-group">
 <label for="Correo"> Correo </label>
-<input type="text" class="form-control" name="correo" value="{{ isset($producto->correo) ? $producto->correo : ''  }}" id="correo">
+<input type="text" class="form-control" name="correo" value="{{ isset($proveedor->correo) ? $proveedor->correo : ''  }}" id="correo">
 </div>
 <div class="form-group">
 <label for="Telefono"> Telefono</label>
-<input type="text" class="form-control" name="telefono" value="{{ isset($producto->telefono) ? $producto->telefono : ''  }}" id="telefono">
+<input type="text" class="form-control" name="telefono" value="{{ isset($proveedor->telefono) ? $proveedor->telefono : ''  }}" id="telefono">
 </div>
 
 <input class="btn btn-success" type="submit" value="{{ $modo }} datos" >
